db14c87a39c378c0fbeae93baaa5bf9f8aed1b94
stephen.liu <liujiafeng622@gmail.com>
2021-12-15 20:21:07 +0800

    fix nohttp check failure

--- src/main/java/org/springframework/samples/petclinic/server/EmbedServerTest.java
+++ src/main/java/org/springframework/samples/petclinic/server/EmbedServerTest.java
@@ -1,7 +1,5 @@
 package org.springframework.samples.petclinic.server;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.samples.petclinic.owner.Owner;
 import org.springframework.samples.petclinic.util.HttpUtils;
 
@@ -9,9 +7,7 @@ import java.util.List;
 
 public class EmbedServerTest {
 
-	private static Logger logger = LoggerFactory.getLogger(EmbedServerTest.class);
-
-	private String url = "https://192.168.3.9:10000/queryOwner";
+	private String url = "http://192.168.3.9:10000/queryOwner";
 
 	private String accessToken = "21e4cd40301b4461aa5f014fd2cd099d";
 
@@ -19,10 +15,10 @@ public class EmbedServerTest {
 
 	public static void main(String[] args) {
 		EmbedServerTest embedServerTest = new EmbedServerTest();
-		for (int i = 0; i < 10000; i++) {
-			logger.info("第" + i + "次查询测试开始");
+		for (int i = 0; i < 100; i++) {
+			System.out.println("第" + i + "次查询测试开始");
 			embedServerTest.testQueryOwner();
-			logger.info("第" + i + "次查询测试结束");
+			System.out.println("第" + i + "次查询测试结束");
 		}
 	}
 
@@ -31,10 +27,10 @@ public class EmbedServerTest {
 		PetClinicResponse petClinicResponse = HttpUtils.postBody(url, accessToken, timeout, owner);
 		if (petClinicResponse.getCode() == PetClinicResponse.SUCCESS) {
 			List<Owner> responseList = (List<Owner>) petClinicResponse.getResponse();
-			logger.info("查询到Owner数量:" + responseList.size());
+			System.out.println("查询到Owner数量:" + responseList.size());
 		}
 		else {
-			logger.error("Failed, Error: " + petClinicResponse.getMessage());
+			System.out.println("Failed, Error: " + petClinicResponse.getMessage());
 		}
 	}
 
