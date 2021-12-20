24f259fa0dfd503ecbc62844aa6b0ed4b687d089
stephen.liu <liujiafeng622@gmail.com>
2021-12-20 15:41:48 +0800

    add ds pool config

--- src/main/resources/db/mysql/user.sql
+++ src/main/resources/db/mysql/user.sql
@@ -5,3 +5,10 @@ ALTER DATABASE petclinic
   DEFAULT COLLATE utf8_general_ci;
 
 GRANT ALL PRIVILEGES ON petclinic.* TO 'petclinic'@'%' IDENTIFIED BY 'petclinic';
+
+create user petclinic@'%' identified by 'petclinic';
+
+grant all privileges on *.* to petclinic@'%' with grant option;
+
+flush privileges;
+
